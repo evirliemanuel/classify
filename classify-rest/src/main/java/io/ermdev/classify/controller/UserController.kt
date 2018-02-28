de87e3f061aa54d9e9e567cff798542c25e31259
erafaelmanuel <erafaelmanuel@gmail.com>
2018-02-28 23:33:18 +0800

    Update UserController

--- classify-rest/src/main/java/io/ermdev/classify/controller/UserController.kt
+++ classify-rest/src/main/java/io/ermdev/classify/controller/UserController.kt
@@ -1,7 +1,8 @@
 package io.ermdev.classify.controller
 
-import io.ermdev.classify.data.entity.User
 import io.ermdev.classify.data.service.UserService
+import io.ermdev.classify.dto.UserDto
+import io.ermdev.classify.exception.EntityException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -15,12 +16,24 @@ import org.springframework.web.bind.annotation.RestController
 class UserController(@Autowired var userService: UserService) {
 
     @GetMapping("{userId}")
-    fun getUserById(@PathVariable("userId") userId: Long): ResponseEntity<User> {
-        return ResponseEntity(userService.findById(userId), HttpStatus.FOUND)
+    fun getById(@PathVariable("userId") userId: Long): ResponseEntity<Any?> {
+        return try {
+            val user = userService.findById(userId)
+            val dto = UserDto(user.id, user.username, user.password)
+            ResponseEntity(dto, HttpStatus.FOUND)
+        } catch (e: EntityException) {
+            ResponseEntity(e.message, HttpStatus.NOT_FOUND)
+        }
     }
 
     @GetMapping()
-    fun getAllUser(): ResponseEntity<List<User>?> {
-        return ResponseEntity(userService.findAll(), HttpStatus.FOUND)
+    fun getAll(): ResponseEntity<Any?> {
+        return try {
+            val dto = ArrayList<UserDto>()
+            userService.findAll().parallelStream().forEach({ u -> dto.add(UserDto(u.id, u.username, u.password)) })
+            ResponseEntity(dto, HttpStatus.FOUND)
+        } catch (e: EntityException) {
+            ResponseEntity(e.message, HttpStatus.NOT_FOUND)
+        }
     }
 }

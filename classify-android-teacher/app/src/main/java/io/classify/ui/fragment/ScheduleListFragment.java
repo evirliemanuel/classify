045ff0d9c1122e8bedd1f0a894be530789abc309
gglifekillmetot <lieverandiver@gmail.com>
2018-03-03 17:46:16 +0800

    Update classify-android-teacher Schedule List and User Profile Layout

--- classify-android-teacher/app/src/main/java/io/classify/ui/fragment/ScheduleListFragment.java
+++ classify-android-teacher/app/src/main/java/io/classify/ui/fragment/ScheduleListFragment.java
@@ -24,7 +24,7 @@ public class ScheduleListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mamin_schedulelist, container, false);
+        return inflater.inflate(R.layout.fragment_main_schedulelist, container, false);
     }
 
 }

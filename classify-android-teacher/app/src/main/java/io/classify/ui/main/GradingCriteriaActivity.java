f7675384730bd3bd28f7e117f2aa00352d341422
gglifekillmetot <lieverandiver@gmail.com>
2018-03-03 23:18:57 +0800

    Update classify-android-teacher subject details layout

---
+++ classify-android-teacher/app/src/main/java/io/classify/ui/main/GradingCriteriaActivity.java
@@ -0,0 +1,13 @@
+package io.classify.ui.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GradingCriteriaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_gradingcriteria);
+    }
+}

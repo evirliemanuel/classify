fff6c538764d357320edaccbd840b6df372150f3
erafaelmanuel <erafaelmanuel@gmail.com>
2018-03-16 08:22:46 +0800

    Delete android-teacher

--- classify-android-teacher/app/src/main/java/io/classify/data/model/User.kt
+++
@@ -1,7 +0,0 @@
-package io.classify.data.model
-
-class User(var id: Long = 0,
-
-           var username: String = "",
-
-           var password: String = "")
--- classify-android-teacher/app/src/main/java/io/classify/di/module/TeacherModule.kt
+++
@@ -1,4 +0,0 @@
-package io.classify.di.module
-
-class TeacherModule {
-}

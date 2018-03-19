30798f68d12e1c8fdd3dc4c9ad1e45967eac1083
gglifekillmetot <lieverandiver@gmail.com>
2018-03-20 06:18:04 +0800

    Add Change Password Layout

---
+++ classify-android/app/src/main/java/com/lieverandiver/thesisproject/AcitivityChangePassword.java
@@ -0,0 +1,13 @@
+package com.lieverandiver.thesisproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AcitivityChangePassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_changepassword);
+    }
+}
---
+++ classify-android/app/src/main/java/com/lieverandiver/thesisproject/AcivityConfirmPassword.java
@@ -0,0 +1,13 @@
+package com.lieverandiver.thesisproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AcivityConfirmPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_confirmpassword);
+    }
+}

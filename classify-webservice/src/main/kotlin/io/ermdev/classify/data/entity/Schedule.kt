50e8f2e5c7ad1c368385843cd0923f3331f293a5
erafaelmanuel <erafaelmanuel@gmail.com>
2018-03-22 01:19:41 +0800

    Update schedule

--- classify-webservice/src/main/kotlin/io/ermdev/classify/data/entity/Schedule.kt
+++ classify-webservice/src/main/kotlin/io/ermdev/classify/data/entity/Schedule.kt
@@ -1,4 +1,20 @@
 package io.ermdev.classify.data.entity
 
-class Schedule {
-}
+import java.sql.Time
+import javax.persistence.*
+
+class Schedule(@Id
+               @GeneratedValue(strategy = GenerationType.AUTO)
+               val id: Long = 0,
+
+               val day: String = "",
+
+               val room: String = "",
+
+               val start: Time = Time(System.currentTimeMillis()),
+
+               val end: Time = Time(System.currentTimeMillis()),
+
+               @ManyToOne(cascade = [CascadeType.PERSIST])
+               @JoinColumn(name = "lessonId")
+               val lesson: Lesson = Lesson())

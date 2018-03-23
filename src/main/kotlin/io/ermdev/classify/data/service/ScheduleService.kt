1c9703045d6eb902bc185300f5ad129fe5637e4b
erafaelmanuel <erafaelmanuel@gmail.com>
2018-03-23 14:57:00 +0800

    Update scheduleService

--- src/main/kotlin/io/ermdev/classify/data/service/ScheduleService.kt
+++ src/main/kotlin/io/ermdev/classify/data/service/ScheduleService.kt
@@ -1,7 +1,36 @@
 package io.ermdev.classify.data.service
 
+import io.ermdev.classify.data.entity.Schedule
+import io.ermdev.classify.data.repository.ScheduleRepository
+import io.ermdev.classify.exception.EntityException
 import org.springframework.stereotype.Service
+import org.springframework.util.StringUtils
 
 @Service
-class ScheduleService {
+class ScheduleService(private val scheduleRepository: ScheduleRepository) {
+
+    fun findAll() = scheduleRepository.findAll()
+
+    fun findById(id: Long): Schedule {
+        return scheduleRepository.findById(id)
+                .orElseThrow { EntityException("No schedule entity with id $id exists!") }
+    }
+
+    fun save(schedule: Schedule) {
+        if (StringUtils.isEmpty(schedule.day)) {
+            throw EntityException("day cannot be empty")
+        }
+        if (!schedule.day.matches(Regex("^[a-zA-Z0-9]+$"))) {
+            throw EntityException("day cannot contain special characters")
+        }
+        if (StringUtils.isEmpty(schedule.room)) {
+            throw EntityException("day cannot be empty")
+        }
+        if (!schedule.room.matches(Regex("^[a-zA-Z0-9]+( [a-zA-Z0-9]+)*$"))) {
+            throw EntityException("room cannot contain special characters")
+        }
+        scheduleRepository.save(schedule)
+    }
+
+    fun deleteById(id: Long) = scheduleRepository.deleteById(id)
 }
